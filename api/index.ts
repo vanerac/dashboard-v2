7674de5610b9050179b4c74432466b8841ff7b37
Corentin MAS <corentinmas@yahoo.com>
2022-01-18 14:44:28 +0100

    [dev] Fixed api eslint + prettier

--- api/index.ts
+++ api/index.ts
@@ -1,10 +1,10 @@
 import express from 'express';
-import routes from  './src/entities'
+import routes from './src/entities';
 
 const app = express();
 
 app.use(routes);
 
 app.listen(3000, () => {
-  console.log('Server is running on port 3000');
+    console.log('Server is running on port 3000');
 });
