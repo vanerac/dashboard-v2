a1811709b46fca18dbe792fe9d7553247b1c6cee
Corentin MAS <corentinmas@yahoo.com>
2022-02-01 10:43:14 +0100

    [feature/monorepo] Functional eslint shared config

--- packages/config/eslint-preset.js
+++ packages/config/eslint-preset.js
@@ -1,5 +1,5 @@
 module.exports = {
-  extends: ["next", "prettier"],
+  extends: ["next", "prettier", "eslint:recommended", "plugin:prettier/recommended", "react-app"],
   settings: {
     next: {
       rootDir: ["apps/*/", "packages/*/"],
@@ -7,5 +7,20 @@ module.exports = {
   },
   rules: {
     "@next/next/no-html-link-for-pages": "off",
+    "prettier/prettier": "error",
+    "prefer-destructuring": [
+      "error",
+      {
+        "array": true,
+        "object": true
+      }
+    ],
+    "no-unused-vars": [
+      "error",
+      { "vars": "all", "args": "after-used", "ignoreRestSiblings": true, "varsIgnorePattern": "^\\$", "argsIgnorePattern": "^\\$" }
+    ],
+
+    "no-use-before-define": "off",
   },
+
 };
