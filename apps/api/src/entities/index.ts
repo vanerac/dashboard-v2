f1590b46e9692763743124b9beabe6545819e952
Corentin MAS <corentinmas@yahoo.com>
2022-02-02 22:22:25 +0100

    [feature/api/service-management] Removed parsing from service/

--- apps/api/src/entities/index.ts
+++ apps/api/src/entities/index.ts
@@ -1,12 +1,11 @@
 import { Router } from 'express';
 import AuthRoutes from './auth/auth.routes';
 import ServiceRoutes from './services/service.router';
-import { parseServiceId } from '../tools/service.tools';
 import { parseToken } from '../tools/auth.tools';
 
 const router = Router();
 
 router.use('/auth', parseToken, AuthRoutes);
-router.use('/service', parseServiceId, ServiceRoutes);
+router.use('/service', ServiceRoutes);
 
 export default router;
