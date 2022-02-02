abaf9b2a444279e9064299c6ffd0e9c92c32dae4
Corentin MAS <corentinmas@yahoo.com>
2022-02-02 22:22:44 +0100

    [feature/api/service-management] Fix middleware auth

--- apps/api/src/entities/index.ts
+++ apps/api/src/entities/index.ts
@@ -1,11 +1,11 @@
 import { Router } from 'express';
 import AuthRoutes from './auth/auth.routes';
 import ServiceRoutes from './services/service.router';
-import { parseToken } from '../tools/auth.tools';
+import { parseToken, verifyToken } from '../tools/auth.tools';
 
 const router = Router();
 
 router.use('/auth', parseToken, AuthRoutes);
-router.use('/service', ServiceRoutes);
+router.use('/service', verifyToken, ServiceRoutes);
 
 export default router;
