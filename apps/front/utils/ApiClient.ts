3db6c6fcd5b9bf8292e992c2f081b637535c4f6a
Corentin MAS <corentinmas@yahoo.com>
2022-02-22 22:31:57 +0100

    [fix/api/linking-services] Fixed Client headers not sending right

--- apps/front/utils/ApiClient.ts
+++ apps/front/utils/ApiClient.ts
@@ -2,9 +2,19 @@ import { ApiClient } from '@area/services';
 
 import Cookies from 'universal-cookie';
 
-const cookies = new Cookies();
 export function getClient() {
-    return new ApiClient({
-        TOKEN: cookies.get('API_TOKEN'),
-    });
+    const cookies = new Cookies();
+    const token = cookies.get('API_TOKEN');
+    if (token) {
+        return new ApiClient({
+            TOKEN: cookies.get('API_TOKEN'),
+            HEADERS: {
+                'Content-Type': 'application/json',
+                Accept: 'application/json',
+                Authorization: `Bearer ${token}`,
+            },
+        });
+    } else {
+        return new ApiClient();
+    }
 }
