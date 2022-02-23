ef1d5fd5c768dea9ede28d00211fe7833f888359
Corentin MAS <corentinmas@yahoo.com>
2022-02-23 15:12:18 +0100

    [feature/api/apple-sso] Implemented Apple Artist service

--- apps/api/src/entities/data/artist/artist.service.ts
+++ apps/api/src/entities/data/artist/artist.service.ts
@@ -19,19 +19,19 @@ export class ArtistService {
         throw new Error('Method not implemented.');
     }
     // get artist albums
-    static async getArtistAlbums($accessToken: string, $artistId: string): Promise<Artist> {
+    static async getArtistAlbums($accessToken: string, $artistId: string): Promise<Album[]> {
         throw new Error('Method not implemented.');
     }
     // get artist top tracks
-    static async getArtistTopTracks($accessToken: string, $artistId: string): Promise<Artist> {
+    static async getArtistTopTracks($accessToken: string, $artistId: string): Promise<Track[]> {
         throw new Error('Method not implemented.');
     }
     // get artist related artists
-    static async getArtistRelatedArtists($accessToken: string, $artistId: string): Promise<Artist> {
+    static async getArtistRelatedArtists($accessToken: string, $artistId: string): Promise<Artist[]> {
         throw new Error('Method not implemented.');
     }
     // get artist playlists
-    static async getArtistPlaylists($accessToken: string, $artistId: string): Promise<Artist> {
+    static async getArtistPlaylists($accessToken: string, $artistId: string): Promise<Playlist[]> {
         throw new Error('Method not implemented.');
     }
 }
@@ -401,6 +401,198 @@ export class GoogleArtistService implements ArtistService {
                     provider: 'youtube',
                 };
             });
+        return playlists;
+    }
+}
+
+export class AppleArtistService extends ArtistService {
+    static async getArtistById(accessToken: string, artistId: string): Promise<Artist> {
+        const url = `https://api.music.apple.com/v1/catalog/us/artists/${artistId}`;
+        const response = await fetch(url, {
+            headers: {
+                Authorization: `Bearer ${accessToken}`,
+            },
+        });
+        const json = await response.json();
+        const artist: Artist = {
+            id: json.data.id,
+            name: json.data.attributes.name,
+            image: json.data.attributes.imageUrl,
+            followers: json.data.attributes.followers.total,
+            external_urls: json.data.id,
+
+            provider: 'apple',
+        };
+        return artist;
+    }
+
+    // get user followed artists
+    static async getFollowedArtists(accessToken: string): Promise<Artist[]> {
+        const url = `https://api.music.apple.com/v1/me/following/contains`;
+        const response = await fetch(url, {
+            headers: {
+                Authorization: `Bearer ${accessToken}`,
+            },
+        });
+        const json = await response.json();
+        const artists: Artist[] = json.data.map((artist: any) => {
+            return {
+                id: artist.id,
+                name: artist.attributes.name,
+                image: artist.attributes.imageUrl,
+                followers: artist.attributes.followers.total,
+                external_urls: {
+                    apple: artist.id,
+                },
+                provider: 'apple',
+            };
+        });
+        return artists;
+    }
+    // follow artist
+    static async followArtist(accessToken: string, artistId: string): Promise<Artist> {
+        const url = `https://api.music.apple.com/v1/me/following/contains`;
+        const response = await fetch(url, {
+            method: 'POST',
+            headers: {
+                Authorization: `Bearer ${accessToken}`,
+            },
+            body: JSON.stringify({
+                data: [
+                    {
+                        type: 'artist',
+                        id: artistId,
+                    },
+                ],
+            }),
+        });
+        const json = await response.json();
+        const artist: Artist = {
+            id: json.data[0].id,
+            name: json.data[0].attributes.name,
+            image: json.data[0].attributes.imageUrl,
+            followers: json.data[0].attributes.followers.total,
+            external_urls: json.data[0].id,
+            provider: 'apple',
+        };
+        return artist;
+    }
+
+    // unfollow artist
+    static async unfollowArtist(accessToken: string, artistId: string): Promise<Artist> {
+        const url = `https://api.music.apple.com/v1/me/following/contains`;
+        const response = await fetch(url, {
+            method: 'DELETE',
+            headers: {
+                Authorization: `Bearer ${accessToken}`,
+            },
+            body: JSON.stringify({
+                data: [
+                    {
+                        type: 'artist',
+                        id: artistId,
+                    },
+                ],
+            }),
+        });
+        const json = await response.json();
+        const artist: Artist = {
+            id: json.data[0].id,
+            name: json.data[0].attributes.name,
+            image: json.data[0].attributes.imageUrl,
+            followers: json.data[0].attributes.followers.total,
+            external_urls: json.data[0].id,
+            provider: 'apple',
+        };
+        return artist;
+    }
+    // get artist albums
+    static async getArtistAlbums(accessToken: string, artistId: string): Promise<Album[]> {
+        const url = `https://api.music.apple.com/v1/catalog/us/artists/${artistId}/albums`;
+        const response = await fetch(url, {
+            headers: {
+                Authorization: `Bearer ${accessToken}`,
+            },
+        });
+        const json = await response.json();
+        const albums: Album[] = json.data.map((album: any) => {
+            return {
+                id: album.id,
+                name: album.attributes.name,
+                image: album.attributes.imageUrl,
+                release_date: album.attributes.releaseDate,
+                external_urls: {
+                    apple: album.id,
+                },
+                provider: 'apple',
+            };
+        });
+        return albums;
+    }
+    // get artist top tracks
+    static async getArtistTopTracks(accessToken: string, artistId: string): Promise<Track[]> {
+        const url = `https://api.music.apple.com/v1/catalog/us/artists/${artistId}/top-tracks?limit=10`;
+        const response = await fetch(url, {
+            headers: {
+                Authorization: `Bearer ${accessToken}`,
+            },
+        });
+        const json = await response.json();
+        const tracks: Track[] = json.data.map((track: any) => {
+            return {
+                id: track.id,
+                name: track.attributes.name,
+                image: track.attributes.artwork.url,
+                duration: track.attributes.durationInMillis,
+                external_urls: {
+                    apple: track.id,
+                },
+                provider: 'apple',
+            };
+        });
+        return tracks;
+    }
+    // get artist related artists
+    static async getArtistRelatedArtists(accessToken: string, artistId: string): Promise<Artist[]> {
+        const url = `https://api.music.apple.com/v1/catalog/us/artists/${artistId}/related-artists`;
+        const response = await fetch(url, {
+            headers: {
+                Authorization: `Bearer ${accessToken}`,
+            },
+        });
+        const json = await response.json();
+        const artists: Artist[] = json.data.map((artist: any) => {
+            return {
+                id: artist.id,
+                name: artist.attributes.name,
+                image: artist.attributes.imageUrl,
+                followers: artist.attributes.followers.total,
+                external_urls: {
+                    apple: artist.id,
+                },
+                provider: 'apple',
+            };
+        });
+        return artists;
+    }
+    // get artist playlists
+    static async getArtistPlaylists(accessToken: string, artistId: string): Promise<Playlist[]> {
+        const url = `https://api.music.apple.com/v1/catalog/us/artists/${artistId}/playlists`;
+        const response = await fetch(url, {
+            headers: {
+                Authorization: `Bearer ${accessToken}`,
+            },
+        });
+        const json = await response.json();
+        const playlists: Playlist[] = json.data.map((playlist: any) => {
+            return {
+                id: playlist.id,
+                name: playlist.attributes.name,
+                image: playlist.attributes.imageUrl,
+                external_urls: '',
+                provider: 'apple',
+            };
+        });
         return playlists;
     }
 }
