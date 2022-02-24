f3d1ba015f0a80c42f01a313ac5016da7fc2dce5
Corentin MAS <corentinmas@yahoo.com>
2022-02-24 18:37:47 +0100

    [feature/widget-routes] Added Openapi widget definition

---
+++ packages/services/models/Widget.ts
@@ -0,0 +1,13 @@
+/* istanbul ignore file */
+/* tslint:disable */
+/* eslint-disable */
+
+export type Widget = {
+    id?: string;
+    serviceId?: string;
+    'x'?: number;
+    'y'?: number;
+    width?: number;
+    height?: number;
+    config?: any;
+};
