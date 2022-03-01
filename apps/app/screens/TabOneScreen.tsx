6aa14863640267d183a86cf92e2a456800f032e6
Ellay-k <louis-antoine.koeck@epitech.eu>
2022-03-01 17:04:03 +0100

    [feature/shared-component/tracklist]

--- apps/app/screens/TabOneScreen.tsx
+++ apps/app/screens/TabOneScreen.tsx
@@ -3,12 +3,14 @@ import React from 'react';
 import EditScreenInfo from '../components/EditScreenInfo';
 import { Text, View } from '../components/Themed';
 import { RootTabScreenProps } from '../types';
+import { TrackListTest } from '../../../packages/ui/Lists/TrackListTest';
 // import { Test } from '../../../packages/ui';
 
 export default function TabOneScreen({ navigation: $nav }: RootTabScreenProps<'TabOne'>) {
     return (
         <View style={styles.container}>
             {/*<Test />*/}
+            <TrackListTest />
             <Text style={styles.title}>Tab One</Text>
             <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
             <EditScreenInfo path="/screens/TabOneScreen.tsx" />
