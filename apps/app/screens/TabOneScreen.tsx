8dca09d47053850ae31d4ab1a022eec5a93de113
Ellay-k <louis-antoine.koeck@epitech.eu>
2022-03-03 16:34:00 +0100

    [feature/shared-component/ItemList] Refactoring ItemList. Now TrackList and is generic only for different types of TrackCard (previously TrackListTest2)

--- apps/app/screens/TabOneScreen.tsx
+++ apps/app/screens/TabOneScreen.tsx
@@ -3,27 +3,27 @@ import React from 'react';
 import EditScreenInfo from '../components/EditScreenInfo';
 import { Text, View } from '../components/Themed';
 import { RootTabScreenProps } from '../types';
-import { TrackListTest2, TrackListType } from '../../../packages/ui/Lists/TrackListTest2';
-import { dataTest, ItemListTest } from '../../../packages/ui/Lists/ItemList';
+import { TrackCard, TrackListType } from '../../../packages/ui/Cards/TrackCard';
+import { TrackList } from '../../../packages/ui/Lists/TrackList';
 
-const TrackListWrapper = ({ item }: { item: TrackListType; type: TrackListType }) => (
-    <TrackListTest2 config={{ type: item }} />
-);
-
-const TestComponent = ({ item }: { item: TrackListType }) => {
-    return (
-        <View style={{ backgroundColor: 'red', width: '100%', height: 60 }}>
-            <Text>{item}</Text>
-        </View>
-    );
-};
+// const TrackListWrapper = ({ item }: { item: TrackListType; type: TrackListType }) => (
+//     <TrackListTest2 config={{ type: item }} />
+// );
+//
+// const TestComponent = ({ item }: { item: TrackListType }) => {
+//     return (
+//         <View style={{ backgroundColor: 'red', width: '100%', height: 60 }}>
+//             <Text>{item}</Text>
+//         </View>
+//     );
+// };
 
 export default function TabOneScreen({ navigation: $nav }: RootTabScreenProps<'TabOne'>) {
     return (
         <View style={styles.container}>
             {/*<TrackListTest2 config={{ type: TrackListType.OPTIONS }} />*/}
-            <ItemListTest itemDataList={dataTest} ItemComponent={TrackListWrapper} itemType={TrackListType.TOGGLE} />
-            <ItemListTest itemDataList={dataTest} ItemComponent={TestComponent} itemType={TrackListType.OPTIONS} />
+            <TrackList itemDataList={dataTest} ItemComponent={TrackCard} itemType={TrackListType.TOGGLE} />
+            <TrackList itemDataList={dataTest} ItemComponent={TrackCard} itemType={TrackListType.OPTIONS} />
             <Text style={styles.title}>Tab One</Text>
             <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
             <EditScreenInfo path="/screens/TabOneScreen.tsx" />
--- packages/ui/Lists/TrackList.tsx
+++ packages/ui/Lists/TrackList.tsx
@@ -1,86 +1,50 @@
-import {onClick} from "..";
-import {Playlist, Service, Track} from "../../services";
-import {Image, Text, View} from "react-native";
-import {Client} from "../../global";
+import React from "react";
+import { StyleSheet, FlatList, ScrollView, View } from "react-native";
+import { TrackCard, TrackListType } from "../Cards/TrackCard";
 
-type TrackListProps = {
-  tracks: Track[];
-  onClick: onClick
-};
+// type DataTest = TrackListType[];
+//
+// export const dataTest: DataTest = [
+//   TrackListType.OPTIONS,
+//   TrackListType.OPTIONS,
+//   TrackListType.TOGGLE,
+// ];
 
-type TrackDisplayConfig = {
-  image: boolean;
-  title: boolean;
-  artist: boolean;
-  album: boolean;
-  duration: boolean;
-};
-
-export default function TrackList(props: TrackListProps) {
-  //
-  // return (
-  //       props.tracks.map((track: Track, index: number) => {
-  //         return (
-  //             <View key={index}>
-  //               <TrackListItem track={track} onClick={props.onClick}/>
-  //             </View>
-  //         )
-  //       })
-  // )
+export interface ItemListProps<Item> {
+  itemDataList: Item[];
+  ItemComponent: React.FC<{ item: Item; type: TrackListType }>;
+  itemType: TrackListType;
 }
 
+const DataTrackList = ({ type }: { type: TrackListType }) => (
+  <TrackCard config={{ type: type }} />
+);
 
-export function TrackListItem(props: {track: Track, serviceId: string, onClick: onClick, config?: Partial<TrackDisplayConfig>}) {
-
-    /* Todo:
-     *  - Like
-     *  - Add to playlist
-     *  - Add to queue
-     *  - Go to artist
-     *  - Go to album
-     *  - Play Track
-     */
-
-    // const playTrack = (track: Track) => {
-    //     Client.playback.playTrack({track, serviceId: serviceId });
-    // }
-
-    const likeTrack = (service: Service, track: Track) => {
-        Client.track.likeTrack(
-            track.id,
-            service.id, // Shoulb be service ID
-        );
-    }
-
-    const unLikeTrack = (service: Service, track: Track) => {
-        Client.track.unlikeTrack(
-            track.id,
-            service.id, // Should be service ID
-        );
-    }
-
-    const addToQueue = (track: Track) => {
-        Client.playback.addToQueue({track});
-    }
-
-    const addToPlaylist = (service: Service, track: Track, playlist: Playlist) => {
-        Client.playlist.addToPlaylist(track.id, service.id, playlist.id);
-    }
-
-    const goToAlbum = (track: Track) => {}
-    const goToArtist = (track: Track) => {}
-
-
+export const TrackList = <Item extends any>({
+  itemDataList,
+  ItemComponent,
+  itemType,
+}: ItemListProps<Item>) => {
+  const renderItem = ({ item }: { item: Item }) => (
+    <ItemComponent item={item} type={itemType} />
+  );
 
   return (
-      <View>
-        <Text>{props.track.name}</Text>
-        <Text>{props.track.artist}</Text>
-        <Text>{props.track.album}</Text>
-          <Text>{props.track.duration}</Text>
-          <Image source={{uri: props.track.image}}/>
-          <Text>{props.track.provider}</Text>
-          <Text>{props.track.playable}</Text>
-      </View>
-  )
-}
+    <FlatList
+      style={stylesheet.container}
+      data={itemDataList}
+      renderItem={renderItem}
+      keyExtractor={(_item, index) => index.toString()}
+    />
+  );
+};
+
+const stylesheet = StyleSheet.create({
+  container: {
+    width: "100%",
+  },
+});
+
+// parameter ItemList : ?maxHeight: string, Array<Item>, direction: boolean
+// Item being all types of card we are gonna put in ItemList (TrackCard, PlaylistCard, ServiceCard, etc)
+// flatlist props refreshing et onRefresh
