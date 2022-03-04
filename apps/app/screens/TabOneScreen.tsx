6ebe1ab59f5c70f824faf1328e382639edb7a724
Ellay-k <louis-antoine.koeck@epitech.eu>
2022-03-04 15:45:26 +0100

    [feature/shared-component/tracklist] Deleting data test

--- apps/app/screens/TabOneScreen.tsx
+++ apps/app/screens/TabOneScreen.tsx
@@ -3,18 +3,18 @@ import React from 'react';
 import EditScreenInfo from '../components/EditScreenInfo';
 import { Text, View } from '../components/Themed';
 import { RootTabScreenProps } from '../types';
-import { TrackList } from '../../../packages/ui/Lists/TrackList';
-import { TrackDisplayConfig, TrackListType } from '../../../packages/ui/Cards/TrackCard';
-import { dataTest } from '../../../packages/ui/dataTest';
+// import { TrackList } from '../../../packages/ui/Lists/TrackList';
+// import { TrackDisplayConfig, TrackListType } from '../../../packages/ui/Cards/TrackCard';
+// import { dataTest } from '../../../packages/ui/dataTest';
 
-const testConfig: TrackDisplayConfig = {
-    type: TrackListType.TOGGLE,
-};
+// const testConfig: TrackDisplayConfig = {
+//     type: TrackListType.TOGGLE,
+// };
 
 export default function TabOneScreen({ navigation: $nav }: RootTabScreenProps<'TabOne'>) {
     return (
         <View style={styles.container}>
-            <TrackList trackArray={dataTest} options={testConfig} />
+            {/*<TrackList trackArray={dataTest} options={testConfig} />*/}
             <Text style={styles.title}>Tab One</Text>
             <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
             <EditScreenInfo path="/screens/TabOneScreen.tsx" />
