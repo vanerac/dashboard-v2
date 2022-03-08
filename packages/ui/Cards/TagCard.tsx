e68f01c5ae777c3659b99bb0e3ca9e3b1ab8e811
Ellay-k <louis-antoine.koeck@epitech.eu>
2022-03-08 07:44:32 +0100

    [shared/widget-album] Adding TagCard

---
+++ packages/ui/Cards/TagCard.tsx
@@ -0,0 +1,27 @@
+import React, { useContext, useState } from "react";
+import { Text, StyleSheet, TouchableOpacity } from "react-native";
+import { Tag } from "../../services";
+import { ThemeContext } from "../../../apps/app/constants/ThemeContext";
+
+export const TagCard = ({ tag }: { tag: Tag }) => {
+  const { theme } = useContext(ThemeContext);
+
+  return (
+    <TouchableOpacity
+      style={stylesheet.container}
+      onPress={() => console.log(tag.name)}
+    >
+      <Text style={{ color: theme.text }}>{tag.name}</Text>
+    </TouchableOpacity>
+  );
+};
+
+const stylesheet = StyleSheet.create({
+  container: {
+    height: 20,
+    width: "50",
+    backgroundColor: "transparent",
+    alignSelf: "center",
+    justifyContent: "center",
+  },
+});
